b3b23e7e964b9f02e061ebb36c364f9e1a243e76
sriram99-web <53469893+sriram99-web@users.noreply.github.com>
2021-04-01 15:19:28 +0530

    Update ThemeParkRideController.java

--- src/main/java/com/tg/controller/ThemeParkRideController.java
+++ src/main/java/com/tg/controller/ThemeParkRideController.java
@@ -21,10 +21,18 @@ public class ThemeParkRideController {
     public Iterable<ThemeParkRide> getRides() {
         return themeParkRideRepository.findAll();
     }
-
+    
     @GetMapping(value = "/ride/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ThemeParkRide getRide(@PathVariable long id){
         return themeParkRideRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Invalid ride id %s", id)));
     }
-    
+    @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ThemeParkRide createRide(@Valid @RequestBody ThemeParkRide themeParkRide) {
+        return themeParkRideRepository.save(themeParkRide);
+    }
+
+    @DeleteMapping(value = "/ride/{id}")
+    public void deleteRide(@PathVariable long id){
+        themeParkRideRepository.deleteById(id);
+    }
 }
